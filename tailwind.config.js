d6be98794574d8ce5da1bf1ef49d8f161016ab12
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-12 15:42:24 -0300

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,18 @@
-const primary = {
-  50: '#EEF2FF',
-  100: '#E0E7FF',
-  200: '#C7D2FE',
-  300: '#A5B4FC',
-  400: '#818CF8',
-  500: '#6366F1',
-  600: '#4F46E5',
-  700: '#4338CA',
-  800: '#3730A3',
-  900: '#312E81',
-};
-
-const secondary = {
-  50: '#FFFBEB',
-  100: '#FEF3C7',
-  200: '#FDE68A',
-  300: '#FCD34D',
-  400: '#FBBF24',
-  500: '#F59E0B',
-  600: '#D97706',
-  700: '#B45309',
-  800: '#92400E',
-  900: '#78350F',
-};
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require('tailwindcss/colors');
+const forms = require('@tailwindcss/forms');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary,
-        secondary,
+        primary: colors.sky,
+        secondary: colors.amber,
+        current: 'currentColor',
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [forms],
 };
