c32c69d5017611583a3cec2e51f22a30c1256d66
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 14:15:23 -0300

    customers tests

--- cypress/e2e/website.cy.js
+++ cypress/e2e/website.cy.js
@@ -1,4 +1,5 @@
 import LANG from '../../src/lang/pt-BR';
+import clientList from '../../src/components/customers/clientList';
 
 describe('e2e Tests', () => {
   const env = Cypress.env();
@@ -12,10 +13,17 @@ describe('e2e Tests', () => {
     cy.get("span").contains(env.REACT_APP_PROJECT_NAME).should("exist");
   });
 
-  it('Hero Should Exists', () => {
+  it('Hero', () => {
     cy.get("h1").contains(LANG.HERO.title).should("exist");
     cy.get("p").contains(LANG.HERO.subtitle).should("exist");
     cy.get("button").contains(LANG.HERO.button).should("exist");
     cy.get("button").contains(LANG.HERO.button).should("exist");
+  });
+
+  it('Customers', () => {
+    cy.get("h2").contains(LANG.CUSTOMERS.title).should("exist");
+    clientList.forEach(({name}) => {
+      cy.get("span").contains(name).children('svg').should("exist");
+    })
   });
 });
