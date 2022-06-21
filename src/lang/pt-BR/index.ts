b2718d6dbd94a5bbe11da61ce7a67623def9b577
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 14:04:53 -0300

    add lang

---
+++ src/lang/pt-BR/index.ts
@@ -0,0 +1,15 @@
+const HERO = {
+  title: 'Mensagem para vender o Produto!',
+  subtitle: 'Um paragrafo maior para deixar o visitante com vontade de assinar!',
+  button: 'Cadastrar',
+};
+
+const CUSTOMERS = {
+  title: 'Alguns Clientes',
+};
+
+const LANG = {
+  HERO, CUSTOMERS,
+};
+
+export default LANG;
