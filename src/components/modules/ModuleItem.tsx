871e358d47d33f8fd72c8eac1150dcfdae4d2a53
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 22:53:19 -0300

    fix element

--- src/components/modules/ModuleItem.tsx
+++ src/components/modules/ModuleItem.tsx
@@ -18,7 +18,7 @@ export default function ModuleItem({
     <div className="w-full lg:w-1/4 flex flex-col rounded-t-lg rounded-b-none shadow mb-4">
       <div className="flex-1 overflow-hidden px-8">
         <p className="w-full text-lg text-center py-2">{title}</p>
-        <div className="w-full font-bold text-xl text-center">{subTitle}</div>
+        <p className="w-full font-bold text-xl text-center">{subTitle}</p>
         <p className="text-base py-2">{text}</p>
       </div>
       <div className="flex items-center justify-center py-6">
