380ac8187eac0719d89212e1cf5f7b9fec2f3ad3
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 21:25:25 -0300

    refactor action

--- src/components/action/Action.tsx
+++ src/components/action/Action.tsx
@@ -1,30 +1,16 @@
 import { ReactElement } from 'react';
+import LANG from '../../lang/pt-BR';
+import RegisterButton from '../shared/RegisterButton';
+import Title from '../shared/Title';
 
-export function Action(): ReactElement {
-  const appSite = process.env.REACT_APP_PROJECT_APP_URL;
-
+export default function Action(): ReactElement {
   return (
-    <section className="gradient w-full mx-auto text-center pt-6 pb-12">
-      <h2 className="w-full my-2 text-5xl font-black leading-tight text-center text-white">
-        Escritório Virtual
-      </h2>
-      <div className="w-full mb-4">
-        <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t" />
+    <section className="w-full border-b gradient2 text-center">
+      <div className="container mx-auto">
+        <Title title={LANG.ACTIONS.title} />
+        <h3 className="text-2xl font-base">{LANG.ACTIONS.subtitle}</h3>
+        <RegisterButton className="my-6 text-white" invert />
       </div>
-
-      <h3 className="my-4 text-3xl font-extrabold text-white">
-        Comece a trabalhar de qualquer lugar, a qualquer hora!
-      </h3>
-
-      <button
-        type="button"
-        className="gradient2 mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded my-6 py-4 px-8 shadow-lg"
-        onClick={() => window.location.assign(`${appSite}/registar`)}
-      >
-        Cadastrar
-      </button>
     </section>
   );
 }
-
-export default Action;
