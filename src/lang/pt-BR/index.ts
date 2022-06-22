e9ad5c03fbd78f8e2fc412ab41bc32bb282d390a
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 21:22:39 -0300

    add langs

--- src/lang/pt-BR/index.ts
+++ src/lang/pt-BR/index.ts
@@ -1,7 +1,89 @@
+const LOADING = 'Carregando';
+
+const BUTTON = {
+  register: 'Cadastrar',
+  experiment: 'Testar',
+};
+
+const NAV = {
+  title: 'Entrar',
+  HOME: {
+    title: 'Início',
+    link: '/',
+  },
+  FEATURES: {
+    title: 'Funcionalidades',
+    link: '/funcionalidades',
+  },
+  PLANS: {
+    title: 'Planos',
+    link: '/planos',
+  },
+};
+
+const FOOTER = {
+  LINKS: {
+    title: 'Links',
+    FAQ: {
+      title: 'FAQ',
+      link: '/faq',
+    },
+    HELP: {
+      title: 'Ajuda',
+      link: '/ajuda',
+    },
+    SUPORT: {
+      title: 'Suporte',
+      link: '/suporte',
+    },
+  },
+  LEGAL: {
+    title: 'Legal',
+    TERMS: {
+      title: 'Termos',
+      link: '/termos',
+    },
+    PRIVACITY: {
+      title: 'Privacidade',
+      link: '/privacidade',
+    },
+  },
+  SOCIAL: {
+    title: 'Social',
+    FACEBOOK: {
+      title: 'Facebook',
+      link: 'https://www.facebook.com/',
+    },
+    INSTAGRAM: {
+      title: 'Instagram',
+      link: 'https://www.instagram.com/',
+    },
+    TWITTER: {
+      title: 'Twitter',
+      link: 'https://www.twitter.com/',
+    },
+    LINKEDIN: {
+      title: 'Linkedin',
+      link: 'https://www.linkedin.com/',
+    },
+  },
+  COMPANY: {
+    title: 'Empresa',
+    ABOUTUS: {
+      title: 'Sobre Nós',
+      link: '/sobre-nos',
+    },
+    CONTACT: {
+      title: 'Contato',
+      link: '/contato',
+    },
+  },
+};
+
 const HERO = {
   title: 'Mensagem para vender o Produto!',
-  subtitle: 'Um paragrafo maior para deixar o visitante com vontade de assinar!',
-  button: 'Cadastrar',
+  subtitle:
+    'Um paragrafo maior para deixar o visitante com vontade de assinar!',
 };
 
 const CUSTOMERS = {
@@ -12,8 +94,48 @@ const PRODUCTS = {
   title: 'Seu Escritório Virtual',
 };
 
+const MODULES = {
+  title: 'Módulos',
+  SCHEDULE: {
+    title: 'Agenda',
+    subtitle: 'Lorem ipsum dolor sit amet.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliuam at ipsum eu nunc commodo posuere et sit amet ligula',
+    link: '/registar',
+  },
+  CALENDAR: {
+    title: 'Calendário',
+    subtitle: 'Pellentesque rhoncus nunc ut leo rhoncus.',
+    text: 'Nunc tempor sapien semper dui pretium, lacinia condimentum nibh maximus. Nunc suscipit nulla et metus blandit pellentesque. Maecenas quis fermentum augue, at porttitor magna.',
+    link: '/registar',
+  },
+  PROCESSES: {
+    title: 'Processos',
+    subtitle: 'Quisque mollis nibh vel justo pretium accumsan.',
+    text: 'Maecenas condimentum tortor orci, at posuere est finibus sit amet. Morbi eget nisi vestibulum, rutrum sem eget, porttitor orci.',
+    link: '/registar',
+  },
+};
+
+const PRICING = {
+  title: 'Planos',
+};
+
+const ACTIONS = {
+  title: 'Escritório Virtual',
+  subtitle: 'Comece a trabalhar de qualquer lugar, a qualquer hora!',
+};
+
 const LANG = {
-  HERO, CUSTOMERS, PRODUCTS,
+  LOADING,
+  BUTTON,
+  NAV,
+  FOOTER,
+  HERO,
+  CUSTOMERS,
+  PRODUCTS,
+  MODULES,
+  PRICING,
+  ACTIONS,
 };
 
 export default LANG;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,20 @@
-export enum Routes {
-  Functionalities = '/funcionalidades',
-  Plans = '/planos',
-  Faq = '/faq',
-  Help = '/ajuda',
-  Suport = '/suporte',
-  Terms = '/termos',
-  Privacity = '/privacidade',
-  AboutUs = '/sobre-nos',
-  Contact = '/contato',
-}
+import LANG from '../lang/pt-BR';
+
+const Routes = {
+  Home: LANG.NAV.HOME.link,
+  Features: LANG.NAV.FEATURES.link,
+  Plans: LANG.NAV.PLANS.link,
+  Faq: LANG.FOOTER.LINKS.FAQ.link,
+  Help: LANG.FOOTER.LINKS.HELP.link,
+  Suport: LANG.FOOTER.LINKS.SUPORT.link,
+  Terms: LANG.FOOTER.LEGAL.TERMS.link,
+  Privacity: LANG.FOOTER.LEGAL.PRIVACITY.link,
+  Facebook: LANG.FOOTER.SOCIAL.FACEBOOK.link,
+  Instagram: LANG.FOOTER.SOCIAL.INSTAGRAM.link,
+  Twitter: LANG.FOOTER.SOCIAL.TWITTER.link,
+  LinkedIn: LANG.FOOTER.SOCIAL.LINKEDIN.link,
+  AboutUs: LANG.FOOTER.COMPANY.ABOUTUS.link,
+  Contact: LANG.FOOTER.COMPANY.CONTACT.link,
+};
 
 export default Routes;
