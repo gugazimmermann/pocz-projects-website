8d04e4bd7ba05c15306e1fbeaaef921c2d6d8127
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-21 22:53:10 -0300

    fix element, change button color

--- src/components/pricing/PricingItem.tsx
+++ src/components/pricing/PricingItem.tsx
@@ -23,17 +23,17 @@ export function PricingItem(
         className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-2xl sm:rounded bg-white shadow-sm  hover:shadow my-6"
       >
         <div className="flex-1 overflow-hidden">
-          <div className="p-4 text-2xl font-bold text-center">{title}</div>
+          <p className="p-4 text-2xl font-bold text-center">{title}</p>
           <ul className="w-full text-center text-sm">
             {features.map((feature, i) => <li key={i} className="py-2">{feature}</li>)}
           </ul>
         </div>
         <div className="w-full text-2xl font-bold text-center py-4">
-          {`$${price} `}
+          <span>{`$${price} `}</span>
           <span className="text-base">{priceText}</span>
         </div>
         <div className="flex items-center justify-center mb-6">
-          <RegisterButton link={link} />
+          <RegisterButton className="text-white" link={link} invert />
         </div>
       </div>
     );
@@ -44,13 +44,13 @@ export function PricingItem(
       className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-2xl gradient shadow hover:shadow-lg text-white z-10"
     >
       <div className="flex-1 overflow-hidden">
-        <div className="p-4 text-3xl font-bold text-center">{title}</div>
+        <p className="p-4 text-3xl font-bold text-center">{title}</p>
         <ul className="w-full text-center text-base">
           {features.map((feature, i) => <li key={i} className="py-2">{feature}</li>)}
         </ul>
       </div>
       <div className="w-full text-4xl font-bold text-center py-4">
-        {`$${price} `}
+        <span>{`$${price} `}</span>
         <span className="text-base">{priceText}</span>
       </div>
       <div className="flex items-center justify-center mb-6">
