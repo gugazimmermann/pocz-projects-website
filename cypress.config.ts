d0eb1b0e4e988662b61d849c0c2ae8a7fa9ba7b0
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-23 16:27:28 -0300

    adds e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // chromeWebSecurity: false,
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
